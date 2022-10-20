0ac242c311a65e24f66903a171a5d2ec99388c04
MussaKhan74 <MarkeLoof@gmail.com>
2022-10-20 06:12:49 +0500

    users async thunk

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -3,11 +3,13 @@ const reduxLogger = require('redux-logger');
 const logger = reduxLogger.createLogger();
 const cakeReducer = require('../features/cake/cakeSlice');
 const icecreamReducer = require('../features/icecream/icecreamSlice');
+const userReducer = require('../features/user/userSlice');
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
+    user: userReducer,
   },
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
